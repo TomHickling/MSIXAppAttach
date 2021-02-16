3fccc211b180b7fecf5106a277d63611f6b22f46
Tom Hickling <tom.hickling@microsoft.com>
2021-02-16 17:20:29 +0000

    Add files via upload

--- Create a self signed code signing certificate2.ps1
+++ Create a self signed code signing certificate2.ps1
@@ -1,8 +1,8 @@
 ﻿#Create Cert
 New-SelfSignedCertificate -Type Custom -Subject "CN=<YOUR ORG>, O=<YOUR ORG>, C=<LOCATION>" -KeyUsage DigitalSignature -FriendlyName "<NAME>" -CertStoreLocation "Cert:\CurrentUser\My" -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}")
-#View Thumbprint 16BF0172CCE631C4225389184774B6BBBBBEEEBD
+#Copy the thumbprint GUID and paste into last line in <THUMBPRINT>
 Set-Location Cert:\CurrentUser\My
 Get-ChildItem | Format-Table Subject, FriendlyName, Thumbprint
 #Export to PFX
 $password = ConvertTo-SecureString -String <PASSWORD> -Force -AsPlainText 
-Export-PfxCertificate -cert "Cert:\CurrentUser\My\16BF0172CCE631C4225389184774B6BBBBBEEEBD" -FilePath <PATH TO>\MSIXCodeSigningCert.pfx -Password $password
+Export-PfxCertificate -cert "Cert:\CurrentUser\My\<THUMPRINT>" -FilePath c:\temp\\MSIXCodeSigningCert.pfx -Password $password
